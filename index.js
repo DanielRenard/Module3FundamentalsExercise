ef286728c93dc124949aa27e1f0acdfcbd2812d0
DanielRenard <danieljuderenard@gmail.com>
2023-10-18 14:45:40 -0500

    Added initial index.js file

---
+++ index.js
@@ -0,0 +1,38 @@
+// What are the results of these expressions? (answer first, then use console.log() to check)
+
+// console.log("" + 1 + 0)
+// console.log("" - 1 + 0)
+// console.log(true + false)
+// console.log(!true)
+// console.log(6/ "3")
+// console.log("2" * "3")
+// console.log(4 + 5 + "px")
+// console.log("$" + 4 + 5)
+// console.log("4" - 2)
+// console.log("4px" - 2)
+// console.log("  -9  " + 5)
+// console.log("  -9  " - 5)
+// console.log(null + 1)
+// console.log(undefined + 1)
+// console.log(undefined == null)
+// console.log(undefined === null)
+// console.log(" \t \n" - 2)
+
+// Which of the below are not giving the right answer? Why are they not correct? How can we fix them?
+
+let three = "3"
+let four = "4"
+let thirty = "30"
+
+let addition = three + four
+console.log(addition)
+let multiplication = three * four
+console.log(multiplication)
+let division = three / four
+console.log(division)
+let subtraction = three - four
+console.log(subtraction)
+let lessThan1 = three < four
+console.log(lessThan1)
+let lessThan2 = thirty < four
+console.log(lessThan2)
